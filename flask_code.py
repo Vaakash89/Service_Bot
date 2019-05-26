ad931aca48cd025541d941ebead1b08aa9de1679
Vaakash89 <euphoric.aakash@gmail.com>
2019-05-27 03:28:09 +0530

    initial

--- flask_code.py
+++ flask_code.py
@@ -12,7 +12,14 @@ app = Flask(__name__)
 
 #@app.route('/users/<user_id>', methods = ['GET', 'POST', 'DELETE'])
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def user():   
-    return request.method
+    if(request.method == 'POST'):
+        #data = request.form['user']['userId']
+        content = request.json
+        if(content['inputs'][0]['arguments'][0]['textValue'] == 'euphoric.aakash@gmail.com'):
+            data = {'facebook':"Thanks God"}
+            return json.dumps(data)
     
+if __name__ == "__main__":
+    app.run(debug=True)
