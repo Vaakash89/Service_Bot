3e06cf30600470ed2edd9375fe633ea350e20373
Vaakash89 <euphoric.aakash@gmail.com>
2019-05-28 00:46:00 +0530

    lets do it

--- flask_code.py
+++ flask_code.py
@@ -42,42 +42,6 @@ def results():
     action = req["queryResult"]["queryText"]
     text = {
               "fulfillmentText": "This is a text response",
-              "fulfillmentMessages": [
-                {
-                  "card": {
-                    "title": "card title",
-                    "subtitle": "card text",
-                    "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-                    "buttons": [
-                      {
-                        "text": "button text",
-                        "postback": "https://assistant.google.com/"
-                      }
-                    ]
-                  }
-                }
-              ],
-              "source": "example.com",
-              "payload": {
-                "google": {
-                  "expectUserResponse": True,
-                  "richResponse": {
-                    "items": [
-                      {
-                        "simpleResponse": {
-                          "textToSpeech": "this is a simple response"
-                        }
-                      }
-                    ]
-                  }
-                },
-                "facebook": {
-                  "text": "Hello, Facebook!"
-                },
-                "slack": {
-                  "text": "This is a text response for Slack."
-                }
-              }
             }
     
     if(action.lower() == "chennai" ):
