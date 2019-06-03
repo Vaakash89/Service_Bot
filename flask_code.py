1ec93a6681834c222c4f69dc7360aef0b09991a9
Vaakash89 <euphoric.aakash@gmail.com>
2019-06-04 00:47:43 +0530

    mongo

--- flask_code.py
+++ flask_code.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify ,json, make_response
 from flask import request
 from flask.ext.pymongo import PyMongo
-import os
 
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
