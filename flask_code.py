d14b48aa5014560f6663e6ec4f35336294d92f91
Vaakash89 <euphoric.aakash@gmail.com>
2019-06-09 00:48:18 +0530

    initial

--- flask_code.py
+++ flask_code.py
@@ -33,7 +33,7 @@ def results():
                                                   {
                                                     "type": "postback",
                                                     "payload": "XYZ Car Comp",
-                                                    "title": mongo
+                                                    "title": MONGO_URI
                                                   },
                                                   {
                                                     "type": "postback",
